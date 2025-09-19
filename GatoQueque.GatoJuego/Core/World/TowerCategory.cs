d93f8a0270c49bed01027366ff02d3a832314bde
michiclair <77251557+michiclair@users.noreply.github.com>
2025-09-19 17:18:44 -0300

    feat: add TowerCategory class

---
+++ GatoQueque.GatoJuego/Core/World/TowerCategory.cs
@@ -0,0 +1,11 @@
+namespace GatoQueque.GatoJuego.Core.World;
+
+internal sealed class TowerCategory
+{
+	internal TowerCategory(String name)
+	{
+		Name = name;
+	}
+
+	internal String Name { get; }
+}
