b004826b0f73cd150097f0b35b597cbf1926e623
chespina <77251557+chespina@users.noreply.github.com>
2026-08-26 20:10:52 -0300

    feat: add TextFontIndex as a singleton

--- GatoQueque.GatoJuego/Core/Assets/DependencyInjection.cs
+++ GatoQueque.GatoJuego/Core/Assets/DependencyInjection.cs
@@ -7,6 +7,7 @@ internal static class DependencyInjection
 		internal IServiceCollection AddAssets()
 		{
 			services.AddSingleton<TextureIndex>();
+			services.AddSingleton<TextFontIndex>();
 			return services;
 		}
 	}
