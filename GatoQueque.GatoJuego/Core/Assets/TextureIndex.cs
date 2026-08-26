2c3642d6c41cbb5270dc6488f2b843ceddfbf40a
chespina <77251557+chespina@users.noreply.github.com>
2026-08-26 19:48:17 -0300

    chore: update textures folder path

--- GatoQueque.GatoJuego/Core/Assets/TextureIndex.cs
+++ GatoQueque.GatoJuego/Core/Assets/TextureIndex.cs
@@ -7,5 +7,5 @@ internal sealed class TextureIndex
 	internal Texture TowerPlaceholder { get; } = new(GenerateAssetPath("tower_placeholder.png"));
 
 	private static String GenerateAssetPath(String assetFileName) =>
-		Path.Combine(Directory.GetCurrentDirectory(), "assets", assetFileName);
+		Path.Combine(Directory.GetCurrentDirectory(), "assets", "textures", assetFileName);
 }
