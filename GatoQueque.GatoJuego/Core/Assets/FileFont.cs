c3e81716a5df460c938cac3c518e4d99a0a5ddde
chespina <77251557+chespina@users.noreply.github.com>
2026-08-26 20:10:17 -0300

    feat: add FileFont class for loading fonts from the disk

---
+++ GatoQueque.GatoJuego/Core/Assets/FileFont.cs
@@ -0,0 +1,44 @@
+﻿using Raylib_cs;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GatoQueque.GatoJuego.Core.Assets;
+
+internal sealed class FileFont : TextFont
+{
+	private readonly String _filePath;
+	private Font? _inVram;
+
+	internal FileFont(String filePath)
+	{
+		_filePath = filePath;
+	}
+
+	internal override Font Value
+	{
+		get
+		{
+			LoadToVram();
+			return _inVram.Value;
+		}
+	}
+
+	[MemberNotNull(nameof(_inVram))]
+	internal void LoadToVram()
+	{
+		if (_inVram is not null)
+			return;
+
+		_inVram = Raylib.LoadFont(_filePath);
+		if (!Raylib.IsFontValid(_inVram.Value))
+			throw new AssetLoadException($"Failed to load to the GPU a texture from the file: {_filePath}");
+	}
+
+	internal void Unload()
+	{
+		if (_inVram is null)
+			return;
+
+		Raylib.UnloadFont(_inVram.Value);
+		_inVram = null;
+	}
+}
