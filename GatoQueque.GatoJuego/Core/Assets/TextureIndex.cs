0adf1572cce8be4d099c792e50770b9849f9cae5
chespina <77251557+chespina@users.noreply.github.com>
2026-08-26 21:40:47 -0300

    feat!: use FrozenDictionary for TextureIndex
    Replace the single TowerPlaceholder property with an immutable FrozenDictionary of textures and make TextureIndex enumerable. Adds constructors that accept filenames (IEnumerable and ReadOnlySpan overload), populates a FrozenDictionary of Texture instances, and exposes an indexer and enumerators. Also adds bulk lifecycle methods: LoadToRam, LoadToVram and Unload. GenerateAssetPath remains unchanged.

--- GatoQueque.GatoJuego/Core/Assets/TextureIndex.cs
+++ GatoQueque.GatoJuego/Core/Assets/TextureIndex.cs
@@ -1,10 +1,51 @@
+using System.Collections.Frozen;
 using System.IO;
 
 namespace GatoQueque.GatoJuego.Core.Assets;
 
-internal sealed class TextureIndex
+internal sealed class TextureIndex : IEnumerable<KeyValuePair<String, Texture>>
 {
-	internal Texture TowerPlaceholder { get; } = new(GenerateAssetPath("tower_placeholder.png"));
+	private readonly FrozenDictionary<String, Texture> _textures;
+
+	internal TextureIndex(IEnumerable<String> textureFileNames)
+		: this(textureFileNames.ToArray())
+	{
+	}
+
+	internal TextureIndex(params ReadOnlySpan<String> textureFileNames)
+	{
+		var textures = new Dictionary<String, Texture>(textureFileNames.Length);
+		foreach (var textureFileName in textureFileNames)
+			textures[textureFileName] = new Texture(GenerateAssetPath(textureFileName));
+		_textures = textures.ToFrozenDictionary();
+	}
+
+	internal void LoadToRam()
+	{
+		foreach (var texture in _textures.Values)
+			texture.LoadToRam();
+	}
+
+	internal void LoadToVram()
+	{
+		foreach (var texture in _textures.Values)
+			texture.LoadToVram();
+	}
+
+	internal void Unload()
+	{
+		foreach (var texture in _textures.Values)
+			texture.Unload();
+	}
+
+	internal Texture this[String textureFileName] => _textures[textureFileName];
+
+	IEnumerator<KeyValuePair<String, Texture>> IEnumerable<KeyValuePair<String, Texture>>.GetEnumerator() =>
+		_textures.GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	internal FrozenDictionary<String, Texture>.Enumerator GetEnumerator() => _textures.GetEnumerator();
 
 	private static String GenerateAssetPath(String assetFileName) =>
 		Path.Combine(Directory.GetCurrentDirectory(), "assets", "textures", assetFileName);
