28c873131a625c3465bfae902dda40b919cf6080
chespina <77251557+chespina@users.noreply.github.com>
2026-08-31 00:02:55 -0300

    feat: add Sound class

--- GatoQueque.GatoJuego/Core/Assets/Sound.cs
+++ GatoQueque.GatoJuego/Core/Assets/Sound.cs
@@ -1,71 +1,12 @@
-﻿using Raylib_cs;
-using System.Diagnostics.CodeAnalysis;
+﻿namespace GatoQueque.GatoJuego.Core.Assets;
 
-namespace GatoQueque.GatoJuego.Core.Assets;
-
-internal sealed class Sound : IDisposable
+internal abstract class Sound
 {
-	private readonly String _filePath;
-	private Boolean _disposed;
-	private Raylib_cs.Sound? _inRam;
-
-	internal Sound(String filePath)
-	{
-		_filePath = filePath;
-	}
-
-	[MemberNotNullWhen(true, nameof(_inRam))]
-	internal Boolean IsLoadedInRam => _inRam is not null;
-
-	internal Int32 LastUsedTick { get; private set; }
-
-	internal Raylib_cs.Sound Value
-	{
-		get
-		{
-			LastUsedTick = Environment.TickCount;
-			LoadToRam();
-			return _inRam.Value;
-		}
-	}
-
-	public void Dispose()
-	{
-		if (_disposed)
-			return;
-
-		GC.SuppressFinalize(this);
-		_disposed = true;
-	}
-
-	[MemberNotNull(nameof(_inRam))]
-	internal void LoadToRam()
-	{
-		ObjectDisposedException.ThrowIf(_disposed, this);
-
-		if (IsLoadedInRam)
-			return;
-
-		var inRam = Raylib.LoadSound(_filePath);
-		if (!Raylib.IsSoundValid(inRam))
-			throw new AssetLoadException($"Failed to load to the CPU a sound from the file: {_filePath}");
-
-		_inRam = inRam;
-	}
-
-	internal void Unload()
-	{
-		ObjectDisposedException.ThrowIf(_disposed, this);
-
-		if (!IsLoadedInRam)
-			return;
-
-		Raylib.UnloadSound(_inRam.Value);
-		_inRam = null;
-	}
-
-	~Sound()
-	{
-		Dispose();
-	}
+	internal abstract Single Volume { get; set; }
+	internal abstract Single Pitch { get; set; }
+	internal abstract Single Pan { get; set; }
+	internal abstract void Play();
+	internal abstract void Pause();
+	internal abstract void Resume();
+	internal abstract void Stop();
 }
