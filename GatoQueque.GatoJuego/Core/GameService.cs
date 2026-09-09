fa633a6561fa1a340e909d415d4761bb11531932
chespina <77251557+chespina@users.noreply.github.com>
2026-09-09 04:16:14 -0300

    refactor: modify main loop

--- GatoQueque.GatoJuego/Core/GameService.cs
+++ GatoQueque.GatoJuego/Core/GameService.cs
@@ -1,18 +1,26 @@
+using Raylib_cs;
+
 namespace GatoQueque.GatoJuego.Core;
 
 internal sealed class GameService : BackgroundService
 {
-	private const Int32 MaxFps = 60;
-	private readonly TimeSpan _frameTime = TimeSpan.FromSeconds(1.0 / MaxFps);
-
 	/// <summary>
 	///     Inicia el juego y ejecuta el loop principal.
 	/// </summary>
 	/// <param name="stoppingToken"></param>
-	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	protected override Task ExecuteAsync(CancellationToken stoppingToken)
 	{
-		while (!stoppingToken.IsCancellationRequested)
-			// TODO:
-			await Task.Delay(_frameTime, stoppingToken);
+		Raylib.InitWindow(800, 600, "Gato Juego");
+		Raylib.InitAudioDevice();
+		Raylib.SetTargetFPS(60);
+		Raylib.SetWindowMinSize(800, 600);
+
+		while (!Raylib.WindowShouldClose() && !stoppingToken.IsCancellationRequested)
+		{
+			Raylib.BeginDrawing();
+			Raylib.EndDrawing();
+		}
+
+		return Task.CompletedTask;
 	}
 }
