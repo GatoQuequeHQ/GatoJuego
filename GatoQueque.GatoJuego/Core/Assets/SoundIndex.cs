65a21aa20cb9cc37dd68a0e3c8fc04bab7064de5
chespina <77251557+chespina@users.noreply.github.com>
2026-09-09 04:14:49 -0300

    feat: add SoundIndex class

---
+++ GatoQueque.GatoJuego/Core/Assets/SoundIndex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Frozen;
+using System.IO;
+
+namespace GatoQueque.GatoJuego.Core.Assets;
+
+internal sealed class SoundIndex : IEnumerable<KeyValuePair<String, Sound>>
+{
+	private readonly FrozenDictionary<String, Sound> _sounds;
+
+	internal SoundIndex(IEnumerable<String> soundFileNames)
+		: this(soundFileNames.ToArray())
+	{
+	}
+
+	internal SoundIndex(params ReadOnlySpan<String> soundFileNames)
+	{
+		var sounds = new Dictionary<String, Sound>(soundFileNames.Length);
+		foreach (var soundFileName in soundFileNames)
+			sounds[soundFileName] = new FileSound(GenerateAssetPath(soundFileName));
+		_sounds = sounds.ToFrozenDictionary();
+	}
+
+	internal Sound this[String soundFileName] => _sounds[soundFileName];
+
+	IEnumerator<KeyValuePair<String, Sound>> IEnumerable<KeyValuePair<String, Sound>>.GetEnumerator() =>
+		_sounds.GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	internal FrozenDictionary<String, Sound>.Enumerator GetEnumerator() => _sounds.GetEnumerator();
+
+	private static String GenerateAssetPath(String assetFileName) =>
+		Path.Combine(Directory.GetCurrentDirectory(), "assets", "sounds", assetFileName);
+}
