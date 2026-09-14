de47a91d0f9ed5cb20757f6d6d9e4bbe49962b50
chespina <77251557+chespina@users.noreply.github.com>
2026-09-14 01:58:22 -0300

    feat: add AssetIndex class

---
+++ GatoQueque.GatoJuego/Core/Assets/AssetIndex.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Frozen;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+namespace GatoQueque.GatoJuego.Core.Assets;
+
+public sealed class AssetIndex<TAsset> : IEnumerable<KeyValuePair<String, TAsset>> where TAsset : Asset
+{
+	private readonly FrozenDictionary<String, TAsset> _assets;
+
+	public AssetIndex(
+		Func<String, TAsset> createAsset,
+		String subfolderName,
+		params ReadOnlySpan<String> assetFileNames)
+	{
+		var assets = new Dictionary<String, TAsset>(assetFileNames.Length);
+		foreach (var assetFileName in assetFileNames)
+			assets[assetFileName] = createAsset(GenerateAssetPath(subfolderName, assetFileName));
+		_assets = assets.ToFrozenDictionary();
+	}
+
+	public TAsset this[String assetFileName] => _assets[assetFileName];
+
+	IEnumerator<KeyValuePair<String, TAsset>> IEnumerable<KeyValuePair<String, TAsset>>.GetEnumerator() =>
+		_assets.GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+	public FrozenDictionary<String, TAsset>.Enumerator GetEnumerator() => _assets.GetEnumerator();
+
+	[Pure]
+	private static String GenerateAssetPath(String subfolderName, String assetFileName) =>
+		Path.Combine(Directory.GetCurrentDirectory(), "assets", subfolderName, assetFileName);
+}
